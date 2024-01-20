3d610f77eaed0a2921510863d99e96af4d53346c
Matt Berti <mat.berti@gmail.com>
2024-01-20 20:03:05 +0000

    fix localstorage hydration

--- src/utils/use-local-storage-serialized.tsx
+++ src/utils/use-local-storage-serialized.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const isServer = typeof window === 'undefined'
 
@@ -8,28 +8,37 @@ const useLocalStorage = <T,>(
   serialize: (item: T) => string = JSON.stringify,
   deserialize: (storedItem: string) => T = JSON.parse
 ): [T, typeof setValue] => {
-  console.log('is server', isServer)
-  const [storedValue, setStoredValue] = useState(() => {
+  const [storedValue, setStoredValue] = useState(() => initialValue)
+
+  const initialize = () => {
     try {
       const item = window.localStorage.getItem(key)
 
       if (item) {
-        console.log('load from localstorage', key, item)
+        // console.log('load from localstorage', key, item)
         return deserialize(item)
       }
 
-      console.log('using initial value', initialValue)
+      // console.log('using initial value', initialValue)
       return initialValue
     } catch (error) {
       return initialValue
     }
-  })
+  }
+
+  /* prevents hydration error so that state is only initialized after server is defined */
+  useEffect(() => {
+    if (!isServer) {
+      setStoredValue(initialize())
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const setValue = (value: T): void => {
     try {
       setStoredValue(value)
       window.localStorage.setItem(key, serialize(value))
-      console.log('wrote to localstorage', key, serialize(value))
+      // console.log('wrote to localstorage', key, serialize(value))
     } catch (error) {
       console.error(`Error setting localStorage value for key '${key}'`)
       console.error(error)
--- src/utils/use-local-storage.tsx
+++ src/utils/use-local-storage.tsx
@@ -1,18 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+const isServer = typeof window === 'undefined'
 
 const useLocalStorage = <T,>(
   key: string,
   initialValue: T
 ): [string | T, typeof setValue] => {
-  const [storedValue, setStoredValue] = useState(() => {
+  const [storedValue, setStoredValue] = useState(() => initialValue)
+
+  const initialize = () => {
     try {
-      const item = window.localStorage.getItem(key)
+      const item = window.localStorage.getItem(key) as T
 
       return item ?? initialValue
     } catch (error) {
       return initialValue
     }
-  })
+  }
+
+  /* prevents hydration error so that state is only initialized after server is defined */
+  useEffect(() => {
+    if (!isServer) {
+      setStoredValue(initialize())
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const setValue = (value: T): void => {
     try {
