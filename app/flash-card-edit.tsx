c7ca88512c3ab22bacaa4ecc317fa4fc08f52313
Matt Berti <mat.berti@gmail.com>
2024-02-03 17:37:17 +0700

    edit card

---
+++ app/flash-card-edit.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { Button, SubmitRow, TextInput } from 'matterial'
+import * as React from 'react'
+
+import classes from './flash-cards.module.css'
+import { LANGUAGES, LANGUAGE_MAP } from '@/const'
+import { editCard } from '@/db/cards'
+import CardsContext from '@/contexts/cards-context'
+
+function FlashCardEdit({
+  card,
+  onFinish,
+}: {
+  card: Card
+  onFinish: () => void
+}) {
+  const [_, setCards] = React.useContext(CardsContext)
+  const [state, setState] = React.useState<Card>(card)
+
+  const handleChange = (index: number) => (e: any) =>
+    setState({
+      ...state,
+      lang: { ...state.lang, [LANGUAGES[index]]: e.target.value },
+    })
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    const newCardSet = editCard(card.id, state)
+    setCards(newCardSet)
+    onFinish()
+  }
+
+  return (
+    <div className={classes.flashCard}>
+      <TextInput
+        name={LANGUAGES[0]}
+        value={state.lang[LANGUAGES[0]]}
+        placeholder={LANGUAGE_MAP[LANGUAGES[0]]}
+        required
+        onChange={handleChange(0)}
+      />
+      <TextInput
+        name={LANGUAGES[1]}
+        value={state.lang[LANGUAGES[1]]}
+        placeholder={LANGUAGE_MAP[LANGUAGES[1]]}
+        required
+        onChange={handleChange(1)}
+      />
+      <SubmitRow>
+        <Button variant="contained" color="primary" onClick={handleSubmit}>
+          Submit
+        </Button>
+        <Button onClick={onFinish}>Cancel</Button>
+      </SubmitRow>
+    </div>
+  )
+}
+
+export default FlashCardEdit
