6e6dfa0cda78dcbc469f747015aa1e8e0b96f075
Matt Berti <mat.berti@gmail.com>
2024-02-03 18:01:02 +0700

    shorter id

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,7 +1,10 @@
+// const BASE = '10000000-1000-4000-8000-100000000000'
+const BASE = '10000000-1000-4000-8000-100000000000'
+
 function uuidv4(): string {
   const crypto = window.crypto || (window as any).msCrypto
 
-  return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c: string) =>
+  return BASE.replace(/[018]/g, (c: string) =>
     (
       parseInt(c, 16) ^
       (crypto.getRandomValues(new Uint8Array(1))[0] &
@@ -10,4 +13,18 @@ function uuidv4(): string {
   )
 }
 
-export default uuidv4
+function shortUniqueId(): string {
+  const crypto = window.crypto || (window as any).msCrypto
+
+  const array = new Uint8Array(6) // Adjust the array size for your desired length
+  crypto.getRandomValues(array)
+
+  const base64 = btoa(String.fromCharCode(...array))
+
+  const shortId = base64.replace(/[^a-zA-Z0-9]/g, '').substring(0, 8)
+
+  return shortId
+}
+
+export default shortUniqueId
+export { uuidv4 }
