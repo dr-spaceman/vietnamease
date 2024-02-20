9ee95ab65fe698d2c4d0f6bb67c9f2341d55f8ab
Matt Berti <mat.berti@gmail.com>
2024-02-20 23:04:08 +0700

    add card

---
+++ app/add-card/error.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { Button, Page } from 'matterial'
+import { useEffect } from 'react'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
+  return (
+    <Page noNav>
+      <h2>Something went wrong :(</h2>
+      <p>{error.message}</p>
+      <Button
+        variant="outlined"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </Button>
+    </Page>
+  )
+}
