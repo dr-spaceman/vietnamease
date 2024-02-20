7e372d245b5e8c598a0b61de09a5b13e1a2d4efe
Matt Berti <mat.berti@gmail.com>
2024-02-20 22:58:38 +0700

    fix keyboard controls bug on init

--- src/utils/keyboard-input-active.ts
+++ src/utils/keyboard-input-active.ts
@@ -1,13 +1,21 @@
+/**
+ * Use a DOM attribute to track whether keyboard controls should be active.
+ * Keyboard controls should be deactivated temporarily when the user is
+ * interacting with a form input or menu.
+ */
+
 function setKeyboardInputActive(active: boolean) {
   if (active) {
-    document.body.dataset.keyboardInputActive = 'true'
+    document.body.dataset.keyboardInputInactive = 'false'
   } else {
-    document.body.dataset.keyboardInputActive = 'false'
+    document.body.dataset.keyboardInputInactive = 'true'
   }
 }
 
 function isKeyboardInputActive() {
-  return document.body.dataset.keyboardInputActive === 'true'
+  const activeState = document.body.dataset.keyboardInputInactive
+
+  return !activeState || activeState === 'false'
 }
 
 export { setKeyboardInputActive, isKeyboardInputActive }
