b6264f6d24207925ef78e4250ed34b31a2cd3abb
Matt Berti <mat.berti@gmail.com>
2024-02-27 23:45:09 +0700

    small updates utils config

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -6,3 +6,11 @@ export const toPascalCase = (s: string): string =>
 
 export const toKebabCase = (s: string): string =>
   s.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
+
+export const formatFilename = (phrase: string): string => {
+  return phrase
+    .replace(/\s+/g, '_') // Replace spaces with underscores
+    .replace(/[\/\?<>\\:\*\|"]/g, '') // Remove invalid filename characters
+    .replace(/\s+/g, '') // Remove extra spaces
+    .toLowerCase() // Convert to lowercase to ensure consistency
+}
