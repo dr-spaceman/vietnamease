b0a2a5458aea01c858d985eae72bc9ee11161260
Matt Berti <mat.berti@gmail.com>
2024-04-14 22:14:49 +0700

    adjust blocked page

--- app/(main)/api/blocked/route.ts
+++ app/(main)/api/blocked/route.ts
@@ -1,6 +1,3 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(429)
-  return res.end()
+export function GET() {
+  return new Response('Blocked', { status: 429 })
 }
